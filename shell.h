aac007052e3bc26ed2bd4bed43a9873073357113
Wqkz <guillaume.farina@hotmail.com>
2024-08-16 16:44:23 +0200

    My Library

--- shell.h
+++ shell.h
@@ -1,16 +1,16 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
+#ifndef SHELL_H
+#define SHELL_H
 
-
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
 
 /*----- Prototype -----*/
-void execute(char *command, char **args)
 
-#endif
+void execute(char *command, char **args);
+
+#endif /* SHELL_H */
