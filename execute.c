a503981f6dfefa49a718e07b1be09a8f9ca07c9e
bisoukeiko <87bisou@gmail.com>
2024-08-20 15:15:44 +0200

    execute.c: handle for memory leak

--- execute.c
+++ execute.c
@@ -3,12 +3,13 @@
 /**
  * execute - Spawn a child process to execute the specified command
  *           and executes the command
+ * @full_path: A pointer to full path command
  * @command: An array of command and its arguments
  * @argv: An array containing arguments for the entire program
  * Return: Nothing
  */
 
-void execute(char **command, char **argv)
+void execute(char *full_path, char **command, char **argv)
 {
 	pid_t pid = fork();
 	int status;
@@ -21,13 +22,14 @@ void execute(char **command, char **argv)
 	else if (pid == 0)
 	{
 
-		if (execve(command[0], command, NULL) == -1)
+		if (execve(full_path, command, NULL) == -1)
 		{
 			for (index = 0; command[index] != NULL; index++)
 			{
 				free(command[index]);
 			}
 			free(command);
+			free(full_path);
 
 			perror(argv[0]);
 			exit(EXIT_FAILURE);
